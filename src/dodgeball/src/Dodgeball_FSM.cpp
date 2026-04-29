42db0ce43bb56b880617674c1e5349cdfc50ac69
Daniel Villarreal <dalexv2007@outlook.com>
2026-04-29 15:36:12 -0500

    updated to vec.distance(s) < 1.0, no change in behavior up to this point.

--- src/dodgeball/src/Dodgeball_FSM.cpp
+++ src/dodgeball/src/Dodgeball_FSM.cpp
@@ -161,7 +161,7 @@ private:
                     current_state_ = State::SEARCH; //if ball lost, go back to search
                     RCLCPP_INFO(this->get_logger(), "NAV_TO_INTER -> SEARCH");
                 }
-                else if(vec.distance < 0.8) //if close to intermediate point, switch to nav to kick pos
+                else if(vec.distance < 1.0) //if close to intermediate point, switch to nav to kick pos
                     current_state_ = State::NAV_TO_KICK_POS;
                     RCLCPP_INFO(this->get_logger(), "NAV_TO_INTER -> NAV_TO_KICK_POS");
                 break;            
@@ -178,7 +178,7 @@ private:
                     current_state_ = State::SEARCH; //if ball lost, go back to search
                     RCLCPP_INFO(this->get_logger(), "NAV_TO_KICK_POS -> SEARCH");
                 }
-                else if(vec.distance < 0.8){ //if close to kick position, switch to line up
+                else if(vec.distance < 1.0){ //if close to kick position, switch to line up
                     current_state_ = State::LINE_UP;
                     RCLCPP_INFO(this->get_logger(), "NAV_TO_KICK_POS -> LINE_UP");
                 }
