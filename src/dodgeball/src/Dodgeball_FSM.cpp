08aecaa66a7735a58e26983bf2db407b37e30f82
Daniel Villarreal <dalexv2007@outlook.com>
2026-04-29 15:39:18 -0500

    typo

--- src/dodgeball/src/Dodgeball_FSM.cpp
+++ src/dodgeball/src/Dodgeball_FSM.cpp
@@ -157,8 +157,8 @@ private:
                 double theta_error = normalize_angle(theta_ - vec.bearing); //compute angle to intermediate point and control output
                 twist_cmd.linear.x = distance_pid_.compute(vec.distance, dt); //forward speed based
                 twist_cmd.angular.z = theta_pid_.compute(theta_error, dt); //angular speed to turn towards intermediate point
-                
-                else if(vec.distance < 1.0) //if close to intermediate point, switch to nav to kick pos
+            
+                if(vec.distance < 1.0) //if close to intermediate point, switch to nav to kick pos
                     current_state_ = State::NAV_TO_KICK_POS;
                     RCLCPP_INFO(this->get_logger(), "NAV_TO_INTER -> NAV_TO_KICK_POS");
                 break;            
